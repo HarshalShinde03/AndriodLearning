6f95e3507ded4a0f9e507b7c2c46ad85022ee814
HarshalShinde03 <79398727+HarshalShinde03@users.noreply.github.com>
2022-06-17 07:34:33 +0530

    Add files via upload

---
+++ Text/Text.java
@@ -0,0 +1,19 @@
+package com.example.andriodlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Text_click extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_click);
+        TextView tv = (TextView)findViewById(R.id.textView2);
+        tv.setText("Welcome to Andriod Learning");
+    }
+}
